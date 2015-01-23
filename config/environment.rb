b9911ffea943bd537c9ef94585034ebeb6509679
Jason Harrelson <jason@lookforwardenterprises.com>
2015-01-22 21:23:23 -0600

    Get rid of API for now.

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,10 @@ require 'lotus/setup'
 
 require_relative '../lib/lotus_test'
 
-require_relative '../apps/api/application'
+#require_relative '../apps/api/application'
 require_relative '../apps/web/application'
 
 Lotus::Container.configure do
-  mount Api::Application.new, at: '/api'
+  #mount Api::Application.new, at: '/api'
   mount Web::Application, at: '/'
 end
